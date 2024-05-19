3eb1e15fd0d83d0c56f2a5565ca0748ff1a1f111
tanao0308 <tanao030801@foxmail.com>
2024-05-19 11:55:52 +0800

    change tab into spaces

--- main.cpp
+++ main.cpp
@@ -7,41 +7,41 @@ RWLock rwlock;
 
 void reader(int id)
 {
-        rwlock.lock_read();
+    rwlock.lock_read();
 
-        cout<<id<<" is reading from file..."<<endl;
-        this_thread::sleep_for(chrono::milliseconds(rand()%1000));
+    cout<<id<<" is reading from file..."<<endl;
+    this_thread::sleep_for(chrono::milliseconds(rand()%1000));
 
-        rwlock.unlock_read();
+    rwlock.unlock_read();
 }
 
 void writer(int id)
 {
-        rwlock.lock_write();
+    rwlock.lock_write();
 
-        cout<<id<<" is writing into file..."<<endl;
-        this_thread::sleep_for(chrono::milliseconds(rand()%1500));
+    cout<<id<<" is writing into file..."<<endl;
+    this_thread::sleep_for(chrono::milliseconds(rand()%1500));
 
-        rwlock.unlock_write();
+    rwlock.unlock_write();
 }
 
 int main()
 {
-        srand(time(NULL));
-        thread readers[5];
-        thread writers[5];
+    srand(time(NULL));
+    thread readers[5];
+    thread writers[5];
 
-        for(int i=0;i<5;++i)
-                readers[i] = thread(reader, i);
+    for(int i=0;i<5;++i)
+        readers[i] = thread(reader, i);
 
-        for(int i=0;i<5;++i)
-                writers[i] = thread(writer, i);
+    for(int i=0;i<5;++i)
+        writers[i] = thread(writer, i);
 
-        for(int i=0;i<5;++i)
-                readers[i].join();
+    for(int i=0;i<5;++i)
+        readers[i].join();
 
-        for(int i=0;i<5;++i)
-                writers[i].join();
+    for(int i=0;i<5;++i)
+        writers[i].join();
 
-        return 0;
+    return 0;
 }
--- src/rwlock.cpp
+++ src/rwlock.cpp
@@ -7,46 +7,46 @@ RWLock::RWLock(): reader_count(0), writing(0) {}
 
 void RWLock::lock_read()
 {
-	unique_lock<mutex> lock(mtx);
-	while(!can_read())
-		cond_read.wait(lock);//参数是什么
+    unique_lock<mutex> lock(mtx);
+    while(!can_read())
+        cond_read.wait(lock);//参数是什么
 
-	reader_count++;
+    reader_count++;
 }
 
 void RWLock::unlock_read()
 {
-	unique_lock<mutex> lock(mtx);
+    unique_lock<mutex> lock(mtx);
 
-	reader_count--;
-	if(reader_count == 0)
-		cond_write.notify_one();
+    reader_count--;
+    if(reader_count == 0)
+        cond_write.notify_one();
 }
 
 void RWLock::lock_write()
 {
-	unique_lock<mutex> lock(mtx);
-	while(!can_write())
-		cond_write.wait(lock);
+    unique_lock<mutex> lock(mtx);
+    while(!can_write())
+        cond_write.wait(lock);
 
-	writing = 1;
+    writing = 1;
 }
 
 void RWLock::unlock_write()
 {
-	unique_lock<mutex> lock(mtx);
-	
-	writing = 0;
-	cond_read.notify_all();
-	cond_write.notify_one();
+    unique_lock<mutex> lock(mtx);
+    
+    writing = 0;
+    cond_read.notify_all();
+    cond_write.notify_one();
 }
 
 bool RWLock::can_read()
 {
-	return writing == 0;
+    return writing == 0;
 }
 
 bool RWLock::can_write()
 {
-	return reader_count == 0 && writing == 0;
+    return reader_count == 0 && writing == 0;
 }
